3ff10d83e628d338df46b6602e9c423e243f87d7
S.I.Shevchenko <sergey12491997@gmail.com>
2022-09-23 00:33:30 +0300

    Commit 23.09.2022 0:35

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def all_blogs(request):
+    posts = Post.objects.order_by("-date")[:10]
+    return render(request, 'blog/all_blogs.html', {"posts": posts})
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Project
 
+
 def home(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/home_page.html', {"projects": projects})
-
